2d1f4c9190a31b26f4a935944d399caa6e556c71
Roberto <ralvesj@hotmail.com>
2022-11-14 09:58:50 -0300

    script atualizado

--- acoesyahoo.py
+++ acoesyahoo.py
@@ -1,17 +1,30 @@
 import pandas as pd 
-import yfinance as yf
+import yfinance as yf # caso precise, instale o yfinance com -> pip install yfinance  
 import datetime as dt
 
-perInicio = '2022-08-01'
-perFin = '2022-08-10'
-ticker = ['MGLU3.SA']
+#Período inicial formato yyyy-MM-dd
+perInicio = '2022-08-01' 
+
+#Período final yyyy-MM-dd
+perFin = '2022-08-10' 
+
+#Coloque aqui os tickers da ações que você quer saber o preço
+ticker = ['PETR4.SA','IRBR3.SA'] 
 
 df = yf.download(ticker, start=perInicio, end=perFin, rounding=True);
 df.sort_values(by=['Date'], inplace=True, ascending=False);
 df.reset_index(inplace=True);
 
-# se você quiser adicionar uma coluna de ticker basta descomentar abaixo 
+#Descomente a linha abaixo se quiser todas as colunas: 
 
-# df = pd.concat([pd.Series(ticker, name='Ticker'), df], axis=1)
+#df = pd.concat([df, pd.Series(ticker, name='Ticker')], axis=1)
 
-print(df)
+#acoes = df
+
+#Aqui você tem um exemplo de como trazer colunas específicas:
+
+acoes = df[['Date', 'Close']]
+
+print(acoes)
+
+#execute com -> python acoesyahoo.py
